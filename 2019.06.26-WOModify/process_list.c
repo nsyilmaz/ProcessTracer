4071d69bb35eb4e27f821cb712615d0357efd93f
Nebi Senol YILMAZ <nsyilmaz@gmail.com>
2019-06-26 18:26:51 +0300

    no message

---
+++ 2019.06.26-WOModify/process_list.c
@@ -0,0 +1,198 @@
+#include "defs.h"
+#include "util.h"
+#include "process_list.h"
+#include "request_handler.h"
+
+//free the whole process list
+void freeProcessList(){
+  if(pList.array != NULL){
+    for(int i=0;i<pList.length;i++){
+      free(pList.array[i].name);
+      free(pList.array[i].pid);
+      free(pList.array[i].user);
+      free(pList.array[i].cmdline);
+      free(pList.array[i].ppid);
+      pList.array[i].name = NULL;
+      pList.array[i].pid = NULL;
+      pList.array[i].user = NULL;
+      pList.array[i].cmdline = NULL;
+      pList.array[i].ppid=NULL;
+    }
+    free(pList.array);
+    pList.array = NULL;
+    pList.length = 0;
+  }
+}
+
+// Generate the process list
+void* addProcessList(void* ptr){
+  struct timespec ts;
+  ts.tv_sec=0;
+  ts.tv_nsec=10000000; // 10 milliseconds
+  while(1){
+    nanosleep(&ts, NULL); // to prevent %100 CPU
+    if(processListStateFlag == 0){ //means there is no need to refresh just go idle
+      continue;
+    }
+    else if(processListStateFlag == 2){ //coming from request handler and indicates program ends
+      break;
+    }
+    else{
+      freeProcessList(); // first delete all list
+      int firstTime = 0;
+      struct dirent *de;
+      DIR *dr = opendir("/proc"); // Open proc directory and visit all directories
+      if(dr == NULL){
+        perror("Can't open the dirs");
+        exit(0);
+      }
+      pList.length++; // hold number of processes
+      pList.array = malloc(sizeof(struct process)); //here we take a memory region from heap
+      while((de = readdir(dr)) != NULL){
+            int length = strlen(de->d_name); //read all files/dirs inside the proc one by one
+            char buffer[length+1];
+            FILE *fp1; //For cmd and status files
+            int counterForCmd=0;
+            char line[100];
+            char ch;
+            int flag;
+            char *c;
+            char *f; // return value from fgets
+            char *statusPath ="/proc/"; // It will show the status file
+            char *cmdPath = "/proc/"; // It will show the cmdfile
+            int flagForCommandLine = 0;
+            strncpy(buffer,de->d_name,length);
+            buffer[length] = '\0';
+            if(checkInt(buffer)){ //We are checking whether it is integer since all processes' dirs' names are numeric
+              if(firstTime != 0){ //firstTime indicates that initially we take malloc we filled it then realloc
+                pList.length++;
+                pList.array = realloc(pList.array,pList.length*sizeof(struct process));
+              }
+            firstTime = 1;
+            cats(&cmdPath,de->d_name); //cats function is similar to strcat only difference is use memcpy
+            cats(&cmdPath,"/cmdline");
+            cats(&statusPath,de->d_name);
+            cats(&statusPath,"/status");
+            fp1 = fopen(cmdPath,"r"); // Copy contents of cmdline arguments
+            if(fp1 == NULL){
+              perror("Can't open process' cmdline file: ");
+              exit(0);
+            }
+            pList.array[pList.length-1].cmdline = NULL;
+            while((ch = fgetc(fp1)) != EOF){
+              if(flagForCommandLine == 0){
+              pList.array[pList.length-1].cmdline = malloc(sizeof(char));
+              flagForCommandLine = 1;
+              }
+              if(flagForCommandLine == 1){
+                if(ch == '\0'){
+                  ch = ' ';
+                }
+                pList.array[pList.length-1].cmdline[counterForCmd++]=ch;
+                pList.array[pList.length-1].cmdline = realloc(pList.array[pList.length-1].cmdline,sizeof(char)*(counterForCmd+1));
+              }
+            }
+            pList.array[pList.length-1].cmdline = realloc(pList.array[pList.length-1].cmdline,sizeof(char)*(counterForCmd+1));
+            pList.array[pList.length-1].cmdline[counterForCmd++]= '\0';
+            fclose(fp1); //Open status file
+            fp1 = fopen(statusPath,"r");
+            if( fp1 == NULL ) {
+                perror("Can't open process' status file: ");
+                exit(0);
+              }
+            do{ //parse the file
+              c = fgets(line,99,fp1);
+              if(c != NULL){
+                if(strstr(line,"Name:")){
+                  f = strstr(line,":") +1;
+                  pList.array[pList.length-1].name = malloc(sizeof(char));
+                  for(int i=0;f;){
+                    if(*f == ' ' | *f=='\t'){
+                      f++;
+                    }
+                    else if(*f == '\n'){
+                      pList.array[pList.length-1].name[i] = '\0';
+                      break;
+                    }
+                    else{
+                      pList.array[pList.length-1].name[i++] = *f++;
+                      pList.array[pList.length-1].name = realloc(pList.array[pList.length-1].name,sizeof(char)*(i+1));
+                    }
+                  }
+                }
+                else if(strstr(line,"PPid:")){
+                  f = strstr(line,":") +1;
+                  pList.array[pList.length-1].ppid = malloc(sizeof(char));
+                  for(int i=0;f;){
+                    if(*f == ' ' | *f=='\t'){
+                      f++;
+                    }
+                    else if(*f == '\n'){
+                      pList.array[pList.length-1].ppid[i] = '\0';
+                      break;
+                    }
+                    else{
+                      pList.array[pList.length-1].ppid[i++] = *f++;
+                      pList.array[pList.length-1].ppid = realloc(pList.array[pList.length-1].ppid,sizeof(char)*(i+1));
+                    }
+                  }
+                }
+                else if(strstr(line,"Pid:")){
+                  if(strstr(line,"Tracer") || strstr(line,"PPid")){
+                    continue;
+                  }
+                  f = strstr(line,":") +1;
+                  pList.array[pList.length-1].pid = malloc(sizeof(char));
+                  for(int i=0;f;){
+                    if(*f == ' ' | *f=='\t'){
+                      f++;
+                    }
+                    else if(*f == '\n'){
+                      pList.array[pList.length-1].pid[i] = '\0';
+                      break;
+                    }
+                    else{
+                      pList.array[pList.length-1].pid[i++] = *f++;
+                      pList.array[pList.length-1].pid = realloc(pList.array[pList.length-1].pid,sizeof(char)*(i+1));
+                    }
+                  }
+                }
+                else if(strstr(line,"Uid:")){
+                  f = strstr(line,":") +1;
+                  int firstTabFlag = 0; //in status file uid is splitted by tab character so this is flag to find uid
+                  char *string = malloc(sizeof(char));
+                  for(int i=0;f;){
+                    if(*f == ' ' | *f=='\t'){
+                      if(firstTabFlag){
+                        string[i] = '\0';
+                        break;
+                      }
+                      f++;
+                      firstTabFlag =1;
+                    }
+                    else{
+                      string[i++] = *f++;
+                      string = realloc(string,sizeof(char)*(i+1));
+                    }
+                  }
+                  struct passwd* pw;
+                  if( ( pw = getpwuid( atoi(string) ) ) != NULL ){ //to find owner name, use uid
+                    pList.array[pList.length-1].user = malloc(sizeof(char)*strlen(pw->pw_name)+1);
+                    strncpy(pList.array[pList.length-1].user,pw->pw_name,strlen(pw->pw_name)+1);
+                  }
+                  free(string);
+                  string = NULL;
+                }
+                else{
+                  continue;
+                }
+              }
+            }while(c != NULL);
+            fclose(fp1);
+          }
+        }
+      closedir(dr);
+      processListStateFlag = 0;
+    }
+  }
+}
---
+++ 2019.06.26-WOModify/request_handler.c
@@ -0,0 +1,242 @@
+#include "util.h"
+#include "request_handler.h"
+#include "process_list.h"
+#include "ptrace.h"
+#include "defs.h"
+
+
+
+// AJAX CALL
+void* requestHandler(void *ptr){
+        int *fd_client = (int *)ptr;
+        char buf[2048]; //content sent by browser
+        int fdimg; //for favicon
+        char buffer[5012];
+        memset(buf,0,2048);
+        read(*fd_client, buf, 2047);
+        printf("%s\n",buf);
+        if(!strncmp(buf, "GET /favicon.ico",16)){
+                write(*fd_client,responseHeader,strlen(responseHeader));
+                write(*fd_client," ",1);
+		/*
+          fdimg = open("secrove.ico", O_RDONLY);
+          sendfile(*fd_client,fdimg,NULL,372000); // size, favicon size
+          close(fdimg);
+		*/
+        }
+        else if(!strncmp(buf,"POST /",6)){
+          // xml http request header ayrımı ajax request geldi mi geldiyse orada thread create edilecek
+	         if(strstr(buf,"pid=")){
+	            if(isStartedPtrace == 0){
+              char *c = strstr(buf,"pid=") + 4;
+              char *pid = malloc(sizeof(char));
+              int i=0;
+              struct process* chosenProcess = NULL;
+              while(*c != EOF && *c != '\n'){
+                pid[i++] = *c++;
+                pid = realloc(pid,sizeof(char)*(i+1));
+              }
+              pid[i] = '\0';
+              chosenProcess = searchByPID(pid);
+              if(chosenProcess == NULL){
+                perror("WRONG PID");
+              }
+              else{
+                write(*fd_client,responseHeader,strlen(responseHeader));
+                write(*fd_client,htmlStart,strlen(htmlStart));
+                sprintf(buffer,"<p> Name of Process: %s </p>\n",chosenProcess->name);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<p>Process Id: %s </p>\n",chosenProcess->pid);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<p>Parent' Process Id: %s </p>\n",chosenProcess->ppid);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<p>Process Owner: %s </p>\n",chosenProcess->user);
+                write(*fd_client,buffer,strlen(buffer));
+                if(strlen(chosenProcess->cmdline) == 0){
+                  sprintf(buffer,"<p>CommandLineArguments: </p>\n");
+                }
+                else{
+                  sprintf(buffer,"<p>CommandLineArguments: %s </p>\n",chosenProcess->cmdline);
+                }
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<button onclick = 'nextSyscall()'> Next </button>\n");
+                write(*fd_client,buffer,strlen(buffer));
+                write(*fd_client,xmlSysCallScript,strlen(xmlSysCallScript));
+                write(*fd_client,htmlEnd,strlen(htmlEnd));
+                traced_process = atoi(pid);
+                if(traced_process >= 0){
+		                flagForNext = 0;
+                    int err = pthread_create(&threadAttach,NULL,ptraceAttach,&traced_process);
+                    if(err != 0){
+                      perror("Running Process Tracer By PID Error:");
+                      exit(0);
+                    }
+                    isStartedPtrace = 1;
+              	}
+                free(pid);
+              }
+           }
+	      }
+        else if(strstr(buf, "path=")){
+	          if(isStartedPtrace == 0){
+           	   char *c = strstr(buf,"path=")+5;
+          	   char *path = malloc(sizeof(char)*strlen(c));
+               if(decode(c, path) > 0){
+                 // response + yeni thread oluşumu path için process çalıştırılacak
+		                isStartedPtrace = 1;
+                    write(*fd_client,responseHeader,strlen(responseHeader));
+                    write(*fd_client,htmlStart,strlen(htmlStart));
+                    sprintf(buffer,"<p>Given Path: %s </p>\n",path);
+                    write(*fd_client,buffer,strlen(buffer));
+                    sprintf(buffer,"<button onclick = 'nextSyscall()'> Next </button>\n");
+                    write(*fd_client,buffer,strlen(buffer));
+                    write(*fd_client,xmlSysCallScript,strlen(xmlSysCallScript));
+                    write(*fd_client,htmlEnd,strlen(htmlEnd));
+                    int err = pthread_create(&threadFork,NULL,ptraceFork,path);
+                    flagForNext = 0;
+                    if(err != 0){
+                        perror("Running Process Tracer By PID Error:");
+                        exit(0);
+                    }
+              }
+	          }
+        }
+        else if(strstr(buf, "xml=1")){ // next butonu
+                write(*fd_client,responseHeader,strlen(responseHeader));
+                write(*fd_client,htmlStart,strlen(htmlStart));
+                for(int i=sList.length-1;i>=0;i--){
+                  if(sList.array[i].regs->orig_eax == SYS_write){
+                    sprintf(buffer,"<p>SYS_WRITE</p>");
+                    write(*fd_client,buffer,strlen(buffer));
+                  }
+                  else if(sList.array[i].regs->orig_eax == SYS_read){
+                    sprintf(buffer,"<p>SYS_READ</p>");
+                    write(*fd_client,buffer,strlen(buffer));
+                  }
+                  else if(sList.array[i].regs->orig_eax == SYS_openat){
+                    sprintf(buffer,"<p>SYS_OPENAT</p>");
+                    write(*fd_client,buffer,strlen(buffer));
+                  }
+                  else if(sList.array[i].regs->orig_eax == SYS_close){
+                    sprintf(buffer,"<p>SYS_CLOSE</p>");
+                    write(*fd_client,buffer,strlen(buffer));
+                    sprintf(buffer,"<p>File Descriptor: %lld",sList.array[i].regs->rdi);
+                    write(*fd_client,buffer,strlen(buffer));
+                  }
+                  sprintf(buffer,"<p> register rdx: %lld </p>",sList.array[i].regs->rdx);
+                  write(*fd_client,buffer,strlen(buffer));
+                  sprintf(buffer,"<p> register rax: %lld </p>",sList.array[i].regs->rax);
+                  write(*fd_client,buffer,strlen(buffer));
+                  sprintf(buffer,"<p> register rdi: %lld </p>",sList.array[i].regs->rdi);
+                  write(*fd_client,buffer,strlen(buffer));
+                  sprintf(buffer,"<p> register rbx: %lld </p>",sList.array[i].regs->rbx);
+                  write(*fd_client,buffer,strlen(buffer));
+                  sprintf(buffer,"<p> register rcx: %lld </p>",sList.array[i].regs->rcx);
+                  write(*fd_client,buffer,strlen(buffer));
+                  sprintf(buffer,"<p> register rsi: %lld </p>",sList.array[i].regs->rsi);
+                  write(*fd_client,buffer,strlen(buffer));
+                  if(sList.array[i].data){
+                    sprintf(buffer,"<p> Data: %s </p>",sList.array[i].data);
+                    write(*fd_client,buffer,strlen(buffer));
+                  }
+                  if(sList.array[i].entry_exit_flag == 1){
+                      sprintf(buffer,"<p> System call exit</p>");
+                      write(*fd_client,buffer,strlen(buffer));
+                  }
+                  else if(sList.array[i].entry_exit_flag == 0){
+                      sprintf(buffer,"<p> System call entry </p>");
+                      write(*fd_client,buffer,strlen(buffer));
+                  }
+                }
+                write(*fd_client,htmlEnd,strlen(htmlEnd));
+                flagForNext = 1;
+        }
+        else if(strstr(buf,"xml=2")){
+            processListStateFlag = 1;
+            while(1){
+              if(processListStateFlag == 0){
+                break;
+              }
+            }
+            write(*fd_client,responseHeader,strlen(responseHeader));
+            write(*fd_client,htmlStart,strlen(htmlStart));
+	          write(*fd_client,tableStart,strlen(tableStart));
+            for(int i=0;i<pList.length;i++){
+                write(*fd_client,"<tr>\n",5);
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].name);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].pid);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].ppid);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].user);
+                write(*fd_client,buffer,strlen(buffer));
+            if(pList.array[i].cmdline != NULL){
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].cmdline);
+                write(*fd_client,buffer,strlen(buffer));
+            }
+            else{
+                write(*fd_client,"<td> </td>\n",10);
+            }
+            sprintf(buffer,"<td> <input type=\"radio\" name=\"pid\" value=\"%s\"> </td>",pList.array[i].pid);
+            write(*fd_client,buffer,strlen(buffer));
+            write(*fd_client,"</tr>\n",6);
+            }
+            write(*fd_client,tableEnd,strlen(tableEnd));
+	          write(*fd_client,xmlProcessListScript,strlen(xmlProcessListScript));
+            write(*fd_client,htmlEnd,strlen(htmlEnd));
+        }
+        else if(strstr(buf, "operation=")){
+            char *c = strstr(buf,"operation=")+10;
+            int key = *c - '0';
+            switch (key) {
+              case 0:
+              write(*fd_client,"GOODBYE MY MASTER",17);
+              close(*fd_client);
+              returnForRequestHandler = 1;
+              processListStateFlag = 2;
+              pthread_exit(&returnForRequestHandler);
+              break;
+            }
+          }
+        }
+        else{
+            processListStateFlag = 1;
+            while(1){
+                if(processListStateFlag == 0){
+                  break;
+                }
+            }
+            write(*fd_client,responseHeader,strlen(responseHeader));
+            write(*fd_client,htmlStartWithCSS,strlen(htmlStartWithCSS));
+            write(*fd_client,mainPanelHTML,strlen(mainPanelHTML));
+            write(*fd_client,tableStart,strlen(tableStart));
+            for(int i=0;i<pList.length;i++){
+                write(*fd_client,"<tr>\n",5);
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].name);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].pid);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].ppid);
+                write(*fd_client,buffer,strlen(buffer));
+                sprintf(buffer,"<td> %s </td> \n",pList.array[i].user);
+                write(*fd_client,buffer,strlen(buffer));
+                if(pList.array[i].cmdline != NULL){
+                    sprintf(buffer,"<td> %s </td> \n",pList.array[i].cmdline);
+                    write(*fd_client,buffer,strlen(buffer));
+                }
+                else{
+                    write(*fd_client,"<td> </td>\n",10);
+                }
+                sprintf(buffer,"<td> <input type=\"radio\" name=\"pid\" value=\"%s\"> </td>",pList.array[i].pid);
+                write(*fd_client,buffer,strlen(buffer));
+                write(*fd_client,"</tr>\n",6);
+            }
+            write(*fd_client,tableEnd,strlen(tableEnd));
+            write(*fd_client,xmlProcessListScript,strlen(xmlProcessListScript));
+            write(*fd_client,htmlEnd,strlen(htmlEnd));
+  			}
+        close(*fd_client);
+        returnForRequestHandler = 0;
+        pthread_exit(&returnForRequestHandler);
+}
